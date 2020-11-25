5bcad2b316bdb73432fe8349ed1733a57e17321a
liuzihao <liuzihao@me>
2020-11-25 20:07:32 +0800

    rasource

---
+++ springboot-test-source/src/main/java/com/kueke/springboottestsource/SpringbootTestSourceApplication.java
@@ -0,0 +1,13 @@
+package com.kueke.springboottestsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTestSourceApplication.class, args);
+    }
+
+}
---
+++ springboot-test-source/src/test/java/com/kueke/springboottestsource/SpringbootTestSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kueke.springboottestsource;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
