2152a64f3186a3cdaba0e7c1d78c36295e3f205e
liuzihao <liuzihao@me>
2021-10-01 12:02:49 +0800

    提交

---
+++ springboot-test/src/test/java/com/example/springboottest/SpringbootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottest;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
