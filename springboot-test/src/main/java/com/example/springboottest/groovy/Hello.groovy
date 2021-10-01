6300baa230d126b33033f2ddb232e5922df2d64d
liuzihao <liuzihao@me>
2021-10-01 11:56:40 +0800

    提交

---
+++ springboot-test/src/main/java/com/example/springboottest/groovy/Hello.groovy
@@ -0,0 +1,11 @@
+package com.example.springboottest.groovy
+
+/**
+ @author liuzihao
+ @create 2021-05-06-07:39
+ */
+class Hello {
+    public String tell(){
+        return "im groovy 123"
+    }
+}
---
+++ springboot-test/src/main/resources/groovy/Hello.groovy
@@ -0,0 +1,13 @@
+package groovy
+
+/**
+ @author liuzihao
+ @create 2021-05-06-07:39
+ */
+class Hello {
+    String tell(){
+        int a = 2;
+
+        return "im groovy resources" + a;
+    }
+}
